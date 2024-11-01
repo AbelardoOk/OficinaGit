522291e68f570f5d7cc37f7de12757d1d28991e3
abelardook <abelardoemailprivado@gmail.com>
2024-11-01 08:45:04 -0400

    Nome da modificação

---
+++ script.js
@@ -0,0 +1,21 @@
+// código de exemplo 
+
+// configuração de login 
+// --> git config --global user.name "Seu nome aqui" 
+// --> git config --global user.email "Seu email aqui"
+
+
+
+// 1 --> git init - inicializar o repositório
+// 2 --> git remote add origin "link do repositório aqui"
+// 3 --> git commit -m "nome do commit"
+// 4 --> git branch -M master
+
+
+
+
+
+// 2 --> git commit -m "first commit"
+// git branch -M master
+// git remote add origin https://github.com/AbelardoOk/OficinaGit.git
+// git push -u origin master
